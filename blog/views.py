7281a8b62cda601631b8dad05ec9f505230fe99d
Zapotoczny <63909484+Zapotoczny@users.noreply.github.com>
2022-12-13 23:24:57 +0100

    Add images and 404 page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,33 @@
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from .forms import ImgForm
+from django.views.generic import DetailView
+from django.views.generic import TemplateView
 # Create your views here.
+
+class Image(TemplateView):
+    form = ImgForm
+    template_name = 'blog/image.html'
+
+    def post(self, request, *args, **kwargs):
+        form = ImgForm(request.POST, request.FILES)
+        if form.is_valid():
+            obj = form.save()
+            return HttpResponseRedirect(reverse_lazy('image_display', kwargs={'pk':obj.id}))
+
+        context = self.get_contaxt_data(form=form)
+        return self.render_to_response(context)
+    
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+class ImageDisplay(DetailView):
+    model = Post
+    template_name = 'blog/image_display.html'
+    context_object_name = 'image'
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -11,3 +36,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
+
+def error_404_view(request, exception):
+    data = {"name": "Simple blog"}
+    return render(request, 'blog/404.html', data)
--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5ha5^&l1am5l*fj8x@b-y=ar#ug)%yt6a)2rl+gmgg7jivlw0c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -118,7 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
