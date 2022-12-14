60d653c56c8932da7a1faf93a2a531a16f5c589b
Zapotoczny <63909484+Zapotoczny@users.noreply.github.com>
2022-12-14 16:15:15 +0100

    Change views and form

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
+
+    title = forms.CharField(widget=forms.TextInput(
+                    attrs={'placeholder': 'Max length 200 characters'}))
+
     class Meta:
         model = Post
         fields = ['title', 'text', 'image']
